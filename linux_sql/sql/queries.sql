01afaf96d3b3c2e502e92c064314f44595c7811e
aayushi93 <aayushi9306@gmail.com>
2019-11-29 21:02:57 +0000

    SQL Queries

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -23,14 +23,15 @@ select
 	avg
 		( host_info.total_mem - host_usage.memory_free)/host_info.total_mem) * 100
 ) as used_memory percentage
-FROM host_usage
+
+FROM 
+	host_usage
 INNER JOIN host_info on host_usage.host_id=host_info.id
+
 GROUP BY 
 	host_usage.host_id,
 	host_info.hostname,
 	host_info.total_mem,
 	Date trunc('hour', timestamp) + date_part('minute', timestamp) :: int/5*interval '5 min' as new_timestamp
-ORDER BY host_usage.id
 
-
-
+ORDER BY host_usage.id;
