0e5d3aeb2c92518960095968312f3139b0c1543c
aayushi93 <aayushi9306@gmail.com>
2019-11-30 23:47:38 +0000

    Removing extra new lines

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,37 +1,31 @@
-				/*SQL QUERIES*/
-
+/*SQL QUERIES*/
 --Question 1
-
-select
-	cpu_number,
-	id as host_id,
-	total_mem
-from
-	host_info
+select 
+  cpu_number, 
+  id as host_id, 
+  total_mem 
+from 
+  host_info 
 order by 
-	cpu_number,
-	total_mem desc;
-
-
-
+  cpu_number, 
+  total_mem desc;
 --Question 2
-
-select
-	host_usage.host_id, 
-	host_info.hostname, 
-	host_info.total_mem,
-	avg
-		( host_info.total_mem - host_usage.memory_free)/host_info.total_mem) * 100
-) as used_memory percentage
-
+select 
+  host_usage.host_id, 
+  host_info.hostname, 
+  host_info.total_mem, 
+  avg (
+    host_info.total_mem - host_usage.memory_free
+  )/ host_info.total_mem
+) * 100
+) as used_memory percentage 
 FROM 
-	host_usage
-INNER JOIN host_info on host_usage.host_id=host_info.id
-
+  host_usage 
+  INNER JOIN host_info on host_usage.host_id = host_info.id 
 GROUP BY 
-	host_usage.host_id,
-	host_info.hostname,
-	host_info.total_mem,
-	Date trunc('hour', timestamp) + date_part('minute', timestamp) :: int/5*interval '5 min' as new_timestamp
-
-ORDER BY host_usage.id;
+  host_usage.host_id, 
+  host_info.hostname, 
+  host_info.total_mem, 
+  Date trunc('hour', timestamp) + date_part('minute', timestamp) :: int / 5 * interval '5 min' as new_timestamp 
+ORDER BY 
+  host_usage.id;
