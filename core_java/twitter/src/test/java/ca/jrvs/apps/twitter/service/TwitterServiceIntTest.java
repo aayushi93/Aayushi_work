79ad85db5e6b19505f9b846aa31c5c38496678a6
aayushi93 <aayushi9306@gmail.com>
2019-12-31 19:29:11 +0000

    Completing JavaDoc and reformatting the code

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceIntTest.java
@@ -34,37 +34,38 @@ public class TwitterServiceIntTest {
         this.dao = new TwitterDAO(httpHelper);
         this.twitterService = new TwitterService(dao);
 
-            String hashTag = "#holidayseason";
-            String text1 = "Happy holidays 2019!!" + " " + hashTag + " " + System.currentTimeMillis();
-            String text2 = "Merry Christmas 2019!!" + " " + hashTag + " " + System.currentTimeMillis();
-            Double latitude = 43.7;
-            Double longitude = 79.3;
-            Tweet postTweet1 = TweetUtil.buildTweet(text1, longitude, latitude);
-            Tweet postTweet2 = TweetUtil.buildTweet(text2, longitude, latitude);
-            tweet1 = twitterService.postTweet(postTweet1);
-            tweet2 = twitterService.postTweet(postTweet2);
+        String hashTag = "#holidayseason";
+        String text1 = "Happy holidays 2019!!" + " " + hashTag + " " + System.currentTimeMillis();
+        String text2 = "Merry Christmas 2019!!" + " " + hashTag + " " + System.currentTimeMillis();
+        Double latitude = 43.7;
+        Double longitude = 79.3;
+        Tweet postTweet1 = TweetUtil.buildTweet(text1, longitude, latitude);
+        Tweet postTweet2 = TweetUtil.buildTweet(text2, longitude, latitude);
+        tweet1 = twitterService.postTweet(postTweet1);
+        tweet2 = twitterService.postTweet(postTweet2);
 
-            assertEquals(text1, tweet1.getText());
-            assertNotNull(tweet1.getCoordinates());
-            assertEquals(2, tweet1.getCoordinates().getCoordinatesTweet().size());
-            assertEquals(longitude, tweet1.getCoordinates().getCoordinatesTweet().get(0));
-            assertEquals(latitude, tweet1.getCoordinates().getCoordinatesTweet().get(1));
-        }
-
-
-        @Test
-        public void showTweet() throws Exception{
-            String[] fields = {"created_at", "id_str"};
-            Tweet tweetShow = twitterService.showTweet(tweet1.getIdStr(),fields);
-            assertEquals(tweet1.getText(), tweetShow.getText());
-        }
-//
-        @After
-        public void deleteTweets () {
-            String[] tweetList = {tweet1.getIdStr(), tweet2.getIdStr()};
-            List<Tweet> deletedTweets = twitterService.deleteTweets(tweetList);
-            assertEquals(deletedTweets.get(0).getText(), tweet1.getText());
-            assertEquals(deletedTweets.get(1).getText(), tweet2.getText());
-        }
+        assertEquals(text1, tweet1.getText());
+        assertNotNull(tweet1.getCoordinates());
+        assertEquals(2, tweet1.getCoordinates().getCoordinatesTweet().size());
+        assertEquals(longitude, tweet1.getCoordinates().getCoordinatesTweet().get(0));
+        assertEquals(latitude, tweet1.getCoordinates().getCoordinatesTweet().get(1));
     }
+
+
+    @Test
+    public void showTweet() throws Exception {
+        String[] fields = {"created_at", "id_str"};
+        Tweet tweetShow = twitterService.showTweet(tweet1.getIdStr(), fields);
+        assertEquals(tweet1.getText(), tweetShow.getText());
+    }
+
+    //
+    @After
+    public void deleteTweets() {
+        String[] tweetList = {tweet1.getIdStr(), tweet2.getIdStr()};
+        List<Tweet> deletedTweets = twitterService.deleteTweets(tweetList);
+        assertEquals(deletedTweets.get(0).getText(), tweet1.getText());
+        assertEquals(deletedTweets.get(1).getText(), tweet2.getText());
+    }
+}
 
