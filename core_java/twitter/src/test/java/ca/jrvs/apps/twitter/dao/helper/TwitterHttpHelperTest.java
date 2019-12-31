513dc06b298df7366f24650fcd87e010e9f2ef8a
aayushi93 <aayushi9306@gmail.com>
2019-12-31 19:26:52 +0000

    Completing JavaDoc and reformatting the code

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
@@ -6,39 +6,37 @@ import org.junit.Test;
 
 import java.net.URI;
 
-import static org.junit.Assert.*;
-
 public class TwitterHttpHelperTest {
 
     @Test
-    public void httpPost() throws Exception{
-            String consumerKey = System.getenv("consumerKey");
-            String consumerSecret = System.getenv("consumerSecret");
-            String accessToken = System.getenv("accessToken");
-            String tokenSecret = System.getenv("tokenSecret");
+    public void httpPost() throws Exception {
+        String consumerKey = System.getenv("consumerKey");
+        String consumerSecret = System.getenv("consumerSecret");
+        String accessToken = System.getenv("accessToken");
+        String tokenSecret = System.getenv("tokenSecret");
 
-            System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
+        System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
 
-            //create components
-            HttpHelper httpHelper = new TwitterHttpHelper(consumerKey,consumerSecret,accessToken,tokenSecret);
-            HttpResponse response = httpHelper.httpPost(new URI("https://api.twitter.com/1.1/statuses/update.json?status=HappyHolidays"));
-            System.out.println(EntityUtils.toString(response.getEntity()));
-        }
+        //create components
+        HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
+        HttpResponse response = httpHelper.httpPost(new URI("https://api.twitter.com/1.1/statuses/update.json?status=HappyHolidays"));
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
 
-        @Test
-        public void httpGet() throws Exception{
-            String consumerKey = System.getenv("consumerKey");
-            String consumerSecret = System.getenv("consumerSecret");
-            String accessToken = System.getenv("accessToken");
-            String tokenSecret = System.getenv("tokenSecret");
+    @Test
+    public void httpGet() throws Exception {
+        String consumerKey = System.getenv("consumerKey");
+        String consumerSecret = System.getenv("consumerSecret");
+        String accessToken = System.getenv("accessToken");
+        String tokenSecret = System.getenv("tokenSecret");
 
-            System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
+        System.out.println(consumerKey + "|" + consumerSecret + "|" + accessToken + "|" + tokenSecret);
 
-            //create components
-            HttpHelper httpHelper = new TwitterHttpHelper(consumerKey,consumerSecret,accessToken,tokenSecret);
-            HttpResponse response = httpHelper.httpGet(new URI("https://api.twitter.com/1.1/statuses/update.json?status=1208608028909547520"));
-            System.out.println(EntityUtils.toString(response.getEntity()));
-
-        }
+        //create components
+        HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
+        HttpResponse response = httpHelper.httpGet(new URI("https://api.twitter.com/1.1/statuses/update.json?status=1208608028909547520"));
+        System.out.println(EntityUtils.toString(response.getEntity()));
 
     }
+
+}
